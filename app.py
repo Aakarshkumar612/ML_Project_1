e75a814c09479d8c5872d74c6f272a3b2f682b18
Aakarsh Kumar <aakarsh612@gmail.com>
2025-11-16 20:28:07 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.ml_project.exception import CustomException
 from src.ml_project.components.data_ingestion import DataIngestion
 from src.ml_project.components.data_ingestion import DataIngestionConfig    
 from src.ml_project.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.ml_project.components.model_trainer import ModelTrainerConfig, ModelTrainer
  
 
 import sys
@@ -17,7 +18,12 @@ if __name__ == "__main__":
         
         #data_transformation=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_array, test_array, preprocessor_obj_path=data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        
+        # Model Training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array, test_array))
+        
         
     except Exception as e:
         logging.info("Custom Exception")
--- src/ml_project/utils.py
+++ src/ml_project/utils.py
@@ -4,11 +4,11 @@ from src.ml_project.exception import CustomException
 from src.ml_project.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
 import pymysql
 import pickle
 import numpy as np
-
-
+from sklearn.metrics import r2_score
 
 load_dotenv()
 
@@ -50,4 +50,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+            
+            #model.fit(X_train, y_train)   # train model
+            
+            y_train_pred = model.predict(X_train)
+            
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)    
